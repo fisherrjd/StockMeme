569911b90f5a979bf0d12fb5b691e07279d2ab6e
jade fisher <fisherrjd@gmail.com>
2024-03-07 20:22:30 +0000

    redo bot no more stock all about gomble

---
+++ Deck.py
@@ -0,0 +1,42 @@
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f"{self.suit} of {self.rank}"
+
+
+class Deck:
+    def __init__(self) -> None:
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        ranks = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+
+        # Create the Deck some how??? Brudder idk what the fuck im doing
+
+        self.cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(rank, suit))
+
+    def deal(self, num_cards):
+        if num_cards > len(self.cards):
+            return None
+        else:
+            dealt_cards = self.cards[:num_cards]
+            self.cards = self.cards[num_cards:]
+            return dealt_cards
+
+    def shuffle(self):
+        import random
+
+        random.shuffle(self.cards)
+
+
+deck = Deck()
+deck.shuffle()
+hand = deck.deal(7)
+print("Dealt cards:")
+for card in hand:
+    print(card)
--- bot.py
+++ bot.py
@@ -25,29 +25,7 @@ async def hello(ctx):
 # Command: !poop
 @bot.command(name="poop", help="Poop In Dr's mouth")
 async def hello(ctx):
-    await ctx.send(f"I want to poop in Dr's mouth!")
-
-
-# Command: !stock <ticker>
-@bot.command(name="stock", help="Get the current stock price for a given ticker")
-async def get_stock_price(ctx, ticker):
-    try:
-        # Get stock data
-        stock_data = yf.Ticker(ticker)
-
-        # Get the current stock price
-        current_price = stock_data.info["ask"]
-
-        # Send the response to the Discord channel
-        await ctx.send(f"The current stock price of {ticker} is: {current_price}")
-    except Exception as e:
-        await ctx.send(f"Error retrieving stock price: {e}")
-
-
-# Command: !stock <ticker>
-@bot.command(name="info", help="Get the latest news on a stock from its ticker")
-async def get_stock_price(ctx, ticker):
-    await ctx.send(f"Currently a WIP... \n{ticker} has no data yet")
+    await ctx.send(f"I want to poop in Dr's mouth   !")
 
 
 # Run the bot with the token
